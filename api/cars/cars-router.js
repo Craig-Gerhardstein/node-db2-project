13d1ce976f3e06bfd6a2328f81a86ca6ea43ba6d
Craig Gerhardstein <77256669+Craig-Gerhardstein@users.noreply.github.com>
2021-05-12 00:10:19 -0400

    mvp

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -28,7 +28,12 @@ checkCarPayload,
 checkVinNumberValid, 
 checkVinNumberUnique,
   async ( req, res, next) => {
-    res.json('posting new car')
+    try {
+        const car = await Car.create(req.body)
+        res.json(car)
+    } catch (err) {
+        next(err)
+    }
 })
 
 module.exports = router
